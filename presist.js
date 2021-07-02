f670938ffa127039059c6e252f1da16772afdb98
Reid <75095757+Reidemption@users.noreply.github.com>
2021-07-02 12:19:10 -0600

    attaching to database

--- presist.js
+++ presist.js
@@ -1,7 +1,24 @@
 const mongoose = require("mongoose");
+const db = mongoose.connection;
 
-function connect(callback) {
-  callback();
+function connect() {
+  let connectionString = `mongodb+srv://reid_gubler:AHg0i8fGhb8Gh8va@cluster0.tmhn8.mongodb.net/Redd-it_mock_app?retryWrites=true&w=majority`;
+  console.log("connecting to db..");
+  mongoose
+    .connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch((err) => {
+      console.log("Error connecting to mongo: ", err);
+    });
 }
 
-module.exports = { connect };
+function onConnect(callback) {
+  db.once("open", callback);
+}
+
+module.exports = {
+  connect,
+  onConnect,
+};
