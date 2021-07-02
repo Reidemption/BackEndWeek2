63e0de54423f549c2a28a669e04df017f911cb7f
Reid <75095757+Reidemption@users.noreply.github.com>
2021-07-02 12:26:09 -0600

    fixing bugs

--- presist.js
+++ presist.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db = mongoose.connection;
 
-function connect() {
+function connect(callback) {
   let connectionString = `mongodb+srv://reid_gubler:AHg0i8fGhb8Gh8va@cluster0.tmhn8.mongodb.net/Redd-it_mock_app?retryWrites=true&w=majority`;
   console.log("connecting to db..");
   mongoose
@@ -12,9 +12,6 @@ function connect() {
     .catch((err) => {
       console.log("Error connecting to mongo: ", err);
     });
-}
-
-function onConnect(callback) {
   db.once("open", callback);
 }
 
