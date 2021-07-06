4cd4181897a90db4a321195b38d1e8b8fb26800b
Reid <75095757+Reidemption@users.noreply.github.com>
2021-07-06 10:09:53 -0600

    Model given in class

--- model.js
+++ model.js
@@ -1,15 +1,27 @@
-const store = {};
 const mongoose = require("mongoose");
-const threadSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  author: String,
-  category: String,
-});
 
-const Thread = mongoose.model("Thread", todoSchema);
+const postSchema = new mongoose.Schema(
+  {
+    author: String,
+    body: String,
+    thread_id: { type: mongoose.Schema.Types.ObjectId, ref: "Thread" },
+  },
+  { timestamps: true }
+);
+
+const threadSchema = new mongoose.Schema(
+  {
+    name: String,
+    description: String,
+    author: String,
+    posts: [postSchema],
+    category: String,
+  },
+  { timestamps: true }
+);
+
+const Thread = mongoose.model("Thread", threadSchema);
 
 module.exports = {
   Thread,
-  store,
 };
